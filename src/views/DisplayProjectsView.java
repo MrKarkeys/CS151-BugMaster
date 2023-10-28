14032bdf7d32364b60a566887f1f3ef0557f9d7f
Karthik Renuprasad <karthikrenuprasad@gmail.com>
2023-10-27 21:00:06 -0700

    small updates cleaning code

--- src/views/DisplayProjectsView.java
+++ src/views/DisplayProjectsView.java
@@ -1,5 +1,4 @@
 
-
 package views;
 
 import java.util.*;
@@ -13,45 +12,45 @@ import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import models.Project;
 
-public class DisplayProjectsView extends Base{
+public class DisplayProjectsView extends Base {
 
-    public BorderPane render(Button home, Button viewProj, Button projForm, Button viewTic, Button ticForm, Button comForm) {
-    	
-    	//nav bar
-        BorderPane mainPane = createBase(home, viewProj, projForm, viewTic, ticForm, comForm);
-        viewProj.setStyle("-fx-background-color: WHEAT");
+	public BorderPane render(Button home, Button viewProj, Button projForm, Button viewTic, Button ticForm,
+			Button comForm) {
 
-        // table of projects
-    	BorderPane centerPane = new BorderPane();
-        Label label = new Label("list of projects");
-        label.setStyle("-fx-font: normal bold 50px elephant; -fx-text-fill: darkolivegreen;");
-        centerPane.setTop(label);
-        BorderPane.setAlignment(label, Pos.CENTER);
-        
-        TilePane projectsTable = new TilePane();
-        projectsTable.setHgap(50);
-        projectsTable.setVgap(30);
-        
-        ProjectController controller = new ProjectController();
-        List<Project> Projects = controller.getProjects();
-        for(int i = 0; i < Projects.size(); i++) {
-        	VBox projectBox = new VBox();
-            Label projectName = new Label(Projects.get(i).getName());
-            projectName.setStyle("-fx-font: normal bold 22.5px elephant; -fx-text-fill: darkolivegreen;");
-            Label projectDesc = new Label(Projects.get(i).getDescription());
-            projectDesc.setStyle("-fx-font: normal bold 17.5px elephant; -fx-text-fill: darkolivegreen;");
-            projectDesc.setWrapText(true);
-            projectDesc.setMaxWidth(400);
-            Label projectLabel = new Label(Projects.get(i).getDate());
-            projectLabel.setStyle("-fx-font: normal bold 15px elephant; -fx-text-fill: darkolivegreen;");
-            
-            projectBox.getChildren().addAll(projectName, projectDesc, projectLabel);
-            projectsTable.getChildren().add(projectBox);
-        }
-        centerPane.setCenter(projectsTable);
-        mainPane.setCenter(centerPane);
+		// nav bar
+		BorderPane mainPane = createBase(home, viewProj, projForm, viewTic, ticForm, comForm);
+		viewProj.setStyle("-fx-background-color: WHEAT");
 
-        return mainPane;
-    }
+		// table of projects
+		BorderPane centerPane = new BorderPane();
+		Label label = new Label("list of projects");
+		label.setStyle("-fx-font: normal bold 50px elephant; -fx-text-fill: darkolivegreen;");
+		centerPane.setTop(label);
+		BorderPane.setAlignment(label, Pos.CENTER);
+
+		TilePane projectsTable = new TilePane();
+		projectsTable.setHgap(50);
+		projectsTable.setVgap(30);
+
+		ProjectController controller = new ProjectController();
+		List<Project> Projects = controller.getProjects();
+		for (int i = 0; i < Projects.size(); i++) {
+			VBox projectBox = new VBox();
+			Label projectName = new Label(Projects.get(i).getName());
+			projectName.setStyle("-fx-font: normal bold 22.5px elephant; -fx-text-fill: darkolivegreen;");
+			Label projectDesc = new Label(Projects.get(i).getDescription());
+			projectDesc.setStyle("-fx-font: normal bold 17.5px elephant; -fx-text-fill: darkolivegreen;");
+			projectDesc.setWrapText(true);
+			projectDesc.setMaxWidth(400);
+			Label projectLabel = new Label(Projects.get(i).getDate());
+			projectLabel.setStyle("-fx-font: normal bold 15px elephant; -fx-text-fill: darkolivegreen;");
+
+			projectBox.getChildren().addAll(projectName, projectDesc, projectLabel);
+			projectsTable.getChildren().add(projectBox);
+		}
+		centerPane.setCenter(projectsTable);
+		mainPane.setCenter(centerPane);
+
+		return mainPane;
+	}
 }
-
--- src/views/TicketFormView.java
+++ src/views/TicketFormView.java
@@ -1,104 +1,102 @@
 package views;
-	
-	import java.time.LocalDate;
 
-import java.util.Arrays;
+import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
-
 import controllers.ProjectController;
 import controllers.TicketController;
 import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
-	import javafx.geometry.Pos;
-	import javafx.scene.control.Button;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
-	import javafx.scene.control.Label;
-	import javafx.scene.control.TextArea;
-	import javafx.scene.control.TextField;
-	import javafx.scene.layout.BorderPane;
-	import javafx.scene.layout.HBox;
-	import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
-import models.Project;
-import models.ProjectModel;
 
-	public class TicketFormView  extends Base{
-		public BorderPane render(Button home, Button viewProj, Button projForm, Button viewTic, Button ticForm, Button comForm)   {
-		       
-	        StackPane centerPane = new StackPane();
-	        
-	        //nav bar
-	        BorderPane mainPane = createBase(home, viewProj, projForm, viewTic, ticForm, comForm);
-	        ticForm.setStyle("-fx-background-color: WHEAT");
+public class TicketFormView extends Base {
+	public BorderPane render(Button home, Button viewProj, Button projForm, Button viewTic, Button ticForm,
+			Button comForm) {
 
-	        //labels for text fields and date picker
-	        Label pName = new Label("choose a project");
-	        Label tName= new Label("enter the name");
-	        Label tDesc= new Label("enter the description");
-	        Label filler= new Label(":)");
+		StackPane centerPane = new StackPane();
 
-	        //text fields and date picker for project info
-	        
-	        //CHANGE TO LIST OF PROJECTS
-	        ProjectController projController = new ProjectController();
-	        List<String> allProj = projController.getProjectName();
-	        Collections.sort(allProj);
-			ComboBox<String> combo_box =  new ComboBox<String>(FXCollections.observableList(allProj));
-	        TilePane dropdown = new TilePane(combo_box);
-	        TextField ticketName = new TextField();
-	        TextArea ticketDescription = new TextArea();
-	        DatePicker ticketStartDate= new DatePicker();
-	        ticketStartDate.setValue(java.time.LocalDate.now());
-	        
-	        //box styling here
-	        VBox centerBox = new VBox(20);
-	        centerBox.setPadding(new Insets(10));
-	        centerBox.setAlignment(Pos.CENTER);
-	        dropdown.setAlignment(Pos.CENTER);
-	        
-	        //submit button
-	        
-	        //CHANGE TO TICKETFORMCONTROLLER
-	        Button subTic = new Button("Submit");
-	        subTic.setOnAction(e -> {
-	        	centerBox.getChildren().remove(centerBox.getChildren().size()-1); // clear bottom text on each project addition
-	        	String projectName = combo_box.getValue();
-	        	String name = ticketName.getText();
-	            String description = ticketDescription.getText();
-	            LocalDate localDate = ticketStartDate.getValue();
-	            TicketController controller = new TicketController();
-	            String message = controller.handleSubmitButtonClick(projectName, name, description, localDate);
+		// nav bar
+		BorderPane mainPane = createBase(home, viewProj, projForm, viewTic, ticForm, comForm);
+		ticForm.setStyle("-fx-background-color: WHEAT");
 
-	            Label resultLabel = new Label(message);
-	            centerBox.getChildren().add(resultLabel);
-	        	
-	        	clear(ticketName, ticketDescription, ticketStartDate);
-	        });
-	       
-	        //clear button
-	        Button clearTic = new Button("Clear");  
-	        clearTic.setOnAction(e -> {clear(ticketName, ticketDescription, ticketStartDate);});
-	       
-	        //create scene
-	        HBox buttons = new HBox (20);
-	        buttons.getChildren().addAll(subTic, clearTic);
-	        buttons.setAlignment(Pos.CENTER);
-	        centerBox.getChildren().addAll(pName, dropdown, tName, ticketName, tDesc, ticketDescription, buttons, filler);
-	        centerPane.getChildren().add(centerBox);
-	        mainPane.setCenter(centerPane);
+		// labels for text fields and date picker
+		Label pName = new Label("choose a project");
+		Label tName = new Label("enter the name");
+		Label tDesc = new Label("enter the description");
+		Label filler = new Label(":)");
 
-	        return mainPane;
-		}
-		
-		// clear fields
-		public void clear(TextField ticketName, TextArea ticketDescription, DatePicker ticketStartDate) {
-			ticketName.clear(); 
-			ticketDescription.clear(); 
-			ticketStartDate.setValue(java.time.LocalDate.now());
-		}
+		// text fields and date picker for project info
 
+		// CHANGE TO LIST OF PROJECTS
+		ProjectController projController = new ProjectController();
+		List<String> allProj = projController.getProjectName();
+		Collections.sort(allProj);
+		ComboBox<String> combo_box = new ComboBox<String>(FXCollections.observableList(allProj));
+		TilePane dropdown = new TilePane(combo_box);
+		TextField ticketName = new TextField();
+		TextArea ticketDescription = new TextArea();
+		DatePicker ticketStartDate = new DatePicker();
+		ticketStartDate.setValue(java.time.LocalDate.now());
 
+		// box styling here
+		VBox centerBox = new VBox(20);
+		centerBox.setPadding(new Insets(10));
+		centerBox.setAlignment(Pos.CENTER);
+		dropdown.setAlignment(Pos.CENTER);
+
+		// submit button
+
+		// CHANGE TO TICKETFORMCONTROLLER
+		Button subTic = new Button("Submit");
+		subTic.setOnAction(e -> {
+			centerBox.getChildren().remove(centerBox.getChildren().size() - 1); // clear bottom text on each project
+																				// addition
+			String projectName = combo_box.getValue();
+			String name = ticketName.getText();
+			String description = ticketDescription.getText();
+			LocalDate localDate = ticketStartDate.getValue();
+			TicketController controller = new TicketController();
+			String message = controller.handleSubmitButtonClick(projectName, name, description, localDate);
+
+			Label resultLabel = new Label(message);
+			centerBox.getChildren().add(resultLabel);
+
+			clear(ticketName, ticketDescription, ticketStartDate);
+		});
+
+		// clear button
+		Button clearTic = new Button("Clear");
+		clearTic.setOnAction(e -> {
+			clear(ticketName, ticketDescription, ticketStartDate);
+		});
+
+		// create scene
+		HBox buttons = new HBox(20);
+		buttons.getChildren().addAll(subTic, clearTic);
+		buttons.setAlignment(Pos.CENTER);
+		centerBox.getChildren().addAll(pName, dropdown, tName, ticketName, tDesc, ticketDescription, buttons, filler);
+		centerPane.getChildren().add(centerBox);
+		mainPane.setCenter(centerPane);
+
+		return mainPane;
 	}
+
+	// clear fields
+	public void clear(TextField ticketName, TextArea ticketDescription, DatePicker ticketStartDate) {
+		ticketName.clear();
+		ticketDescription.clear();
+		ticketStartDate.setValue(java.time.LocalDate.now());
+	}
+
+}
